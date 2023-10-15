21a3c9c013e36a845827003467e53d402394620c
Oscar Wiklund <147804906+OscarWiklundJonsson@users.noreply.github.com>
2023-10-15 21:30:58 +0200

    Create kmh_till_knop.py

---
+++ kmh_till_knop.py
@@ -0,0 +1,6 @@
+knop = 1.852
+speed = 0
+kmh = int(input("Skriv hastighet: ")) # Input från användaren
+
+speed = knop * kmh # Multiplicerar omvandlingsfaktorn med inputen(kmh)
+print ((str(knop)) + " knop motsvarar " + (str(speed)) + " km/h") # Skriver ut resultatet
