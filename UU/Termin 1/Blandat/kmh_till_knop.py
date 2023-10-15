e280842852bda24ffa0ecdeba7e49ef87576d82d
Oscar Wiklund <147804906+OscarWiklundJonsson@users.noreply.github.com>
2023-10-15 22:23:53 +0200

    Delete UU/Termin 1/Blandat/kmh_till_knop.py

--- UU/Termin 1/Blandat/kmh_till_knop.py
+++
@@ -1,6 +0,0 @@
-knop = 1.852
-speed = 0
-kmh = int(input("Skriv hastighet: ")) # Input från användaren
-
-speed = knop * kmh # Multiplicerar omvandlingsfaktorn med inputen(kmh)
-print ((str(knop)) + " knop motsvarar " + (str(speed)) + " km/h") # Skriver ut resultatet
