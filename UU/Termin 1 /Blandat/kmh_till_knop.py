e0bfab2faeb178e86dd3a236c646981480b7dd32
Oscar Wiklund Jonsson <oscar.wiklund-jonsson.2717@student.uu.se>
2023-10-18 13:22:05 +0200

    Fixade variabel namn
    Fixade felaktiga variablen namn. Nu är skriptet mer tydligt och förståeligt.

--- UU/Termin 1 /Blandat/kmh_till_knop.py
+++ UU/Termin 1 /Blandat/kmh_till_knop.py
@@ -2,9 +2,9 @@
 # Date: 10/10 - 2023
 # Omvandla från KM/H till Knop
 
-knop = 1.852
-speed = 0
+omvandlingsfaktor = 1.852
+knop = 0
 kmh = int(input("Skriv hastighet i kmh: ")) # Input från användaren
 
-speed = knop * kmh # Multiplicerar omvandlingsfaktorn med inputen(kmh)
-print ((str(knop)) + " knop motsvarar " + (str(speed)) + " km/h") # Skriver ut resultatet
+knop = omvandlingsfaktor * kmh # Multiplicerar omvandlingsfaktorn med inputen(kmh)
+print ((str(kmh)) + " km/h motsvarar " + (str(knop)) + " knop") # Skriver ut resultatet
