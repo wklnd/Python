db487092343c56e092472c65dd9921de1456ee76
John Maple <wiklundoscar45@gmail.com>
2023-11-22 18:35:57 +0100

    Create config.py
    Added a base for a Config.py

---
+++ UU/Termin 1 /DiscordBot/config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Discord Bot Token
+BOT_TOKEN = 'YOUR_DISCORD_BOT_TOKEN_HERE'
+
+# Bot Prefixes
+BOT_PREFIXES = ['!', '?']
+
+# API Keys
+GOOGLE_API_KEY = 'YOUR_GOOGLE_API_KEY_HERE'
+TWITTER_API_KEY = 'YOUR_TWITTER_API_KEY_HERE'
+WEATHER_API_KEY = 'YOUR_WEATHER_API_KEY_HERE'
+# Add other API keys or sensitive information here...
+
+# Other Configurations
+SOME_CONFIG_VARIABLE = 'value'
+ANOTHER_CONFIG_VARIABLE = 'another_value'
